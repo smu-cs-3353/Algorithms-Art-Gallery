7d25ed7277e3c2b92d4355061229513677343342
Logan Schmitt <logan.schmitt@me.com>
2021-09-06 17:17:19 -0500

    Main update

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,12 @@
 
 using namespace std;
 
-<<<<<<< Updated upstream
-int main(int argc, char ** argv) {
-
-    ifstream inputFile(argv[1]);
-    if (!inputFile.is_open()) {
-        cout << "Could not open file" << endl;
-        return -1;
-    }
-
-    Wall wall;
-    wall.createWall(inputFile);
-    wall.printDimensions();
-=======
 int main( int argc, char *argv[] ){
     string inputFileName(argv[1]);
     ArtGallery gallery(inputFileName);
     gallery.displayAllPaintings();
 
 }
->>>>>>> Stashed changes
 
 
 
