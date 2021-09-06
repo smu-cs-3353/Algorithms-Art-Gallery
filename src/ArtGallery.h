88d6d6bbe8fcd5e64980d5d5ccd6ff547b9c0df9
Logan Schmitt <logan.schmitt@me.com>
2021-09-06 17:13:11 -0500

    Added Wall to ArtGallery and updated methods

---
+++ src/ArtGallery.h
@@ -0,0 +1,26 @@
+//
+// Created by tallb on 9/6/2021.
+//
+
+#ifndef INC_21F_PA01_MILLER_AND_SCHMITT_ARTGALLERY_H
+#define INC_21F_PA01_MILLER_AND_SCHMITT_ARTGALLERY_H
+#include "Painting.h"
+#include "Wall.hpp"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+class ArtGallery {
+private:
+    std::vector<Painting> paintings;
+    Wall wall;
+public:
+    ArtGallery();
+    ArtGallery(std::string);
+
+    void displayAllPaintings();
+
+};
+
+
+#endif //INC_21F_PA01_MILLER_AND_SCHMITT_ARTGALLERY_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+<<<<<<< Updated upstream
 int main(int argc, char ** argv) {
 
     ifstream inputFile(argv[1]);
@@ -15,6 +16,14 @@ int main(int argc, char ** argv) {
     Wall wall;
     wall.createWall(inputFile);
     wall.printDimensions();
+=======
+int main( int argc, char *argv[] ){
+    string inputFileName(argv[1]);
+    ArtGallery gallery(inputFileName);
+    gallery.displayAllPaintings();
+
+}
+>>>>>>> Stashed changes
 
 
 
