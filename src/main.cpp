94ff8ca437c2085a5ce53fdeaaad2c0fba25e65d
millerbm18 <millerbm@smu.edu>
2021-09-09 14:00:06 -0500

    checking command line args

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 int main( int argc, char *argv[] ){
+    if(argc!=2){
+        cout<<"Incorrect number of command line args. Please pass one name of a file inside your input folder."<<endl;
+        return 0;
+    }
     string inputFileName(argv[1]);
     ArtGallery gallery(inputFileName);
     gallery.bruteForce();
