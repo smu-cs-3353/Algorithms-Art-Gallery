aae03af1649205e53caf30e3ebe7e5c4ab30be7f
millerbm18 <millerbm@smu.edu>
2021-09-09 14:01:57 -0500

    command line args check

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main( int argc, char *argv[] ){
     if(argc!=2){
-        cout<<"Incorrect number of command line args. Please pass one name of a file inside your input folder."<<endl;
+        cout<<argc<<"Incorrect number of command line args. Please pass one name of a file inside your input folder."<<endl;
         return 0;
     }
     string inputFileName(argv[1]);
